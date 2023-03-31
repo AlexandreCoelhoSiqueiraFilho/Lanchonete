ad4dd5a5b34756acfb61fed3a7bd190e5aea6e0a
AlexandreCoelhoSiqueiraFilho <alexandrecoelhosiqueirafilho@gmail.com>
2023-03-30 21:59:33 -0300

    Lanchonete em html e css

---
+++ lanchonete.js
@@ -0,0 +1,10 @@
+function dowload(){
+    document.getElementById("trocarimg").src="dowload.jpeg";
+}
+function complementos(){
+    document.getElementById("trocarimg").src="complementos.jpeg";
+}
+
+function refrigerants(){
+    document.getElementById("trocarimg").src="refrigerantes.jpeg";
+}
